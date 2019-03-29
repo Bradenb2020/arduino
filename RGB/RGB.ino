c7192eebb3d444393f229c10b01029c4e92c9952
Bradenb2020 <31696976+Bradenb2020@users.noreply.github.com>
2019-03-29 11:05:47 -0300

    Add files via upload

---
+++ RGB/RGB.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(5, HIGH);
+  delay(500);
+  digitalWrite(3, LOW);
+  delay(500);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(5, LOW);
+  delay(500);
+  digitalWrite(3, HIGH);
+  delay(500);
+  digitalWrite(5, HIGH);
+  delay(500);
+  digitalWrite(3, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(7, LOW);
+  delay(500);
+}
